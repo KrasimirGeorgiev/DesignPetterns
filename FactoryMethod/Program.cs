9687ac741cdf6ff01f68ce1cdf097076f2a8731e
Krasimir Georgiev <krasi.g.georgiev@gmail.com>
2024-08-01 22:29:51 +0300

    Adding definition to Method Factory pattern

--- FactoryMethod/Program.cs
+++ FactoryMethod/Program.cs
@@ -1,1 +1,5 @@
 ﻿
+
+// Method factory is a creational design pattern
+// Defines an interface for creating a single object, but let subclasses decide which class to instantiate. 
+// It let's a class defer instantiation to subclasses.
