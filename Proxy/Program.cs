cdc522b56a193165b1ef00e9c1eca3f9e704766b
Krasimir Georgiev <krasi.g.georgiev@gmail.com>
2024-08-10 22:47:47 +0300

    Adding a prototype pattern

---
+++ Proxy/Program.cs
@@ -0,0 +1,6 @@
+﻿// The Prototype Design Pattern specifies the kind of objects to create using a prototypical instance
+// and creates new object by copying this prototype.
+
+// This pattern is useful when the creation of an object is time-consuming or complex in terms of resources.
+// In stead of going through the costly process of creating a fresh object from scratch, an existing instance serves as a prototype,
+// and new objects can be created by copying this prototype.
