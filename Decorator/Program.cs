b317b9fb994d7dd17a60109ac7a49a1d7be84039
Krasimir Georgiev <krasi.g.georgiev@gmail.com>
2024-08-29 23:29:15 +0300

    Adding Decorator Pattern example

---
+++ Decorator/Program.cs
@@ -0,0 +1,7 @@
+﻿// Decorator design pattern allows us to dynamicaly add new functionalities to an exising object without altering
+//  or modifying it's structure, and this desing pattern acts as a wrapper to the existing class. That means the 
+//  decorator design pattern dynamically changes the functionality of an object at runtime without impacting
+//  the existing functionality of the object. In short the pattern adds aditional functionalities to an object by wrapping it.
+//  A decorator is an object that adds features to another object.
+
+
